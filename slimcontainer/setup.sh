29839ab0df06d0947c1a31f7b2f1e5eaaae02518
Staffan Malmgren <staffan.malmgren@kahnpedersen.se>
2023-06-20 16:18:06 +0200

    seems we need to run celery in the same process as uswgi in order for the former to be able to restart the latter

--- slimcontainer/setup.sh
+++ slimcontainer/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-echo "creating tables in postgres"
+echo "creating the tables in postgres"
 python -m docassemble.webapp.create_tables /usr/share/docassemble/config/config.yml www-data
-echo "everything is ready to start for real. running the real CMD ($@)"
+echo "everything is ready to start for realz. running the real CMD ($@)"
 exec "$@"
